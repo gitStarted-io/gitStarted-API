f74b91611be3a2413986030c8ad63aee1734a726
Jake Alsemgeest <alsemgeester@gmail.com>
2016-05-01 19:43:31 -0400

    Removed useless files for API.

---
+++ routes.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+module.exports = function(app) {
+
+	// Just to check if it's alive.
+	app.get('/', require('./routes/index.js'));
+
+	// GET REQUESTS
+	
+
+	// POST REQUESTS
+	
+
+};
