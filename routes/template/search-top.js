db4ba327ba8e0f0505322e773219ca36fc75b8c0
zharley18 <zackharley@gmail.com>
2016-05-02 22:42:05 -0400

    Add GET endpoints. Still has an error.

---
+++ routes/template/search-top.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = (function() {
+'use strict';
+
+	router.get('/search/top', function(req, res, next) {
+	  // res.send('<h1>It\'s ALIVE!</h1>');
+	});
+
+})();
---
+++ routes/template/template-download.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = (function() {
+'use strict';
+
+	router.get('/:template/download', function(req, res, next) {
+	  // res.send('<h1>It\'s ALIVE!</h1>');
+	});
+
+})();
