ea0e4879206dfbe3cf857752f19f87101c376131
Jake Alsemgeest <alsemgeester@gmail.com>
2016-05-15 14:59:43 -0400

    Just updated for when I'm doing User dev.

--- routes/user/user-info.js
+++ routes/user/user-info.js
@@ -6,7 +6,10 @@ module.exports = (function() {
 	var router = express.Router();
 
 	router.get('/user/:user', function(req, res, next) {
-		res.send('<h1>Hi there, '+req.params.user+'</h1>');
+		res.send({
+			id:1,
+			username:req.params.user
+		});
 	});
 
 	return router;
