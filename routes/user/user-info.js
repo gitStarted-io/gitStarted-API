a365cdd18096d720a565e68abbf73ece66ddf010
Jake Alsemgeest <alsemgeester@gmail.com>
2016-05-15 15:16:50 -0400

    Updated with needed information.

--- routes/user/user-info.js
+++ routes/user/user-info.js
@@ -8,7 +8,38 @@ module.exports = (function() {
 	router.get('/user/:user', function(req, res, next) {
 		res.send({
 			id:1,
-			username:req.params.user
+			username:req.params.user,
+			thumbnail:'https://assets-cdn.github.com/images/modules/logos_page/Octocat.png',
+			templates:[
+				{
+					templateId: 1,
+					templateName: 'express-is-awesome',
+					description: 'An express template with some cool plugins.',
+					tags: [
+						'express',
+						'jade'
+					],
+					version: '1.0.3',
+					upvotes: 120,
+					downvotes: 3,
+					author: req.params.user
+				},
+				{
+					templateId: 2,
+					templateName: 'gitstarted',
+					description: 'The best template of them all.',
+					tags: [
+						'gitStarted',
+						'react',
+						'sql',
+						'express'
+					],
+					version: '0.0.1',
+					upvotes: 10000000000,
+					downvotes: 0,
+					author: req.params.user
+				}
+			]
 		});
 	});
 
